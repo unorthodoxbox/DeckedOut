45773b3c41b8e76a7e6a43a5b744dafae6886c16
boko-z <155576498+boko-z@users.noreply.github.com>
2025-04-09 15:55:00 -0400

    Ground Color & Sky Tint Changes
    Added ground color changes based on d/n; tweaked sky tint values; tweaked fog

--- Assets/Scripts/Lighting/LightingController.cs
+++ Assets/Scripts/Lighting/LightingController.cs
@@ -12,8 +12,18 @@ public class LightingController : MonoBehaviour
 
     [SerializeField] private Material skyboxMaterial;
 
-    [SerializeField] private float dayAtmosphereThickness = 0.45f; // daytime value (6-18)
-    [SerializeField] private float nightAtmosphereThickness = 0.22f; // nighttime value (0-3, 21-24)
+    [SerializeField] private float dayAtmosphereThickness = 0.45f; // daytime value (7.5-16.5)
+    [SerializeField] private float nightAtmosphereThickness = 0.22f; // nighttime value (0-4.5, 19.5-24)
+    [SerializeField] private Color daySkyTint = new Color32(0x06, 0x6F, 0x6C, 0xFF);
+    [SerializeField] private Color nightSkyTint = new Color32(0x33, 0x00, 0xC0, 0xFF);
+    [SerializeField] private Color dayGroundColor = new Color32(0x75, 0x64, 0x98, 0xFF);
+    [SerializeField] private Color nightGroundColor = new Color32(0x16, 0x08, 0x33, 0xFF);
+
+    void Start()
+    {
+        Debug.Log("Current skybox material: " + RenderSettings.skybox);
+        Debug.Log("Skybox shader: " + RenderSettings.skybox.shader.name);
+    }
 
     // Update is called once per frame
     void Update()
@@ -55,6 +65,7 @@ public class LightingController : MonoBehaviour
         }
 
         UpdateAtmosphereThickness(timePercent);
+        UpdateSkyboxColors(timePercent);
     }
 
     //helper method to change atmosphere thickness - used for dusk/dawn & night adjustments
@@ -64,26 +75,12 @@ public class LightingController : MonoBehaviour
         {
             if (timePercent >= 0.1875 && timePercent <= 0.3125) //dawn transitions
             {
-                //0.158 - 0.221
-                //if (timePercent >= 0.158 && timePercent <= 0.221)
-                //{
-                //    float slowLerp = Mathf.InverseLerp(0.158, 0.221, timePercent);
-                //    float slowThickness = Mathf.Lerp(nightAtmosphereThickness, dayAtmosphereThickness, slowLerp);
-                //    skyboxMaterial.SetFloat("_AtmosphereThickness", slowThickness);
-                //}
                 float lerpValue = Mathf.InverseLerp(0.1875f, 0.3125f, timePercent);
                 float thickness = Mathf.Lerp(nightAtmosphereThickness, dayAtmosphereThickness, lerpValue);
                 skyboxMaterial.SetFloat("_AtmosphereThickness", thickness);
             }
             else if (timePercent >= 0.6875 && timePercent <= 0.8125) //dusk transitions
             {
-                //0.779 - 0.841
-                //if (timePercent >= 0.779 && timePercent <= 0.841)
-                //{
-                //    float slowLerp = Mathf.InverseLerp(0.779, 0.841, timePercent);
-                //    float slowThickness = Mathf.Lerp(nightAtmosphereThickness, dayAtmosphereThickness, slowLerp);
-                //    skyboxMaterial.SetFloat("_AtmosphereThickness", slowThickness);
-                //}
                 float lerpValue = Mathf.InverseLerp(0.6875f, 0.8125f, timePercent);
                 float thickness = Mathf.Lerp(dayAtmosphereThickness, nightAtmosphereThickness, lerpValue);
                 skyboxMaterial.SetFloat("_AtmosphereThickness", thickness);
@@ -95,6 +92,40 @@ public class LightingController : MonoBehaviour
             else if (timePercent < 0.1875 || timePercent > 0.8125) //night
             {
                 skyboxMaterial.SetFloat("_AtmosphereThickness", nightAtmosphereThickness);
+            }
+        }
+    }
+
+    //updates the skybox ting & ground color based on time of day
+    private void UpdateSkyboxColors(float timePercent)
+    {
+        if (skyboxMaterial != null)
+        {
+            if (timePercent >= 0.1875 && timePercent <= 0.3125) //dawn transitions
+            {
+                float lerpValue = Mathf.InverseLerp(0.1875f, 0.3125f, timePercent);
+                RenderSettings.skybox.SetColor("_SkyTint", Color.Lerp(nightSkyTint, daySkyTint, lerpValue));
+                RenderSettings.skybox.SetColor("_GroundColor", Color.Lerp(nightGroundColor, dayGroundColor, lerpValue));
+                DynamicGI.UpdateEnvironment();
+            }
+            else if (timePercent >= 0.6875 && timePercent <= 0.8125) //dusk transitions
+            {
+                float lerpValue = Mathf.InverseLerp(0.6875f, 0.8125f, timePercent);
+                RenderSettings.skybox.SetColor("_SkyTint", Color.Lerp(daySkyTint, nightSkyTint, lerpValue));
+                RenderSettings.skybox.SetColor("_GroundColor", Color.Lerp(dayGroundColor, nightGroundColor, lerpValue));
+                DynamicGI.UpdateEnvironment();
+            }
+            else if (timePercent > 0.3125 && timePercent < 0.6875) //day
+            {
+                RenderSettings.skybox.SetColor("_SkyTint", daySkyTint);
+                RenderSettings.skybox.SetColor("_GroundColor", dayGroundColor);
+                DynamicGI.UpdateEnvironment();
+            }
+            else if (timePercent < 0.1875 || timePercent > 0.8125) //night
+            {
+                RenderSettings.skybox.SetColor("_SkyTint", nightSkyTint);
+                RenderSettings.skybox.SetColor("_GroundColor", nightGroundColor);
+                DynamicGI.UpdateEnvironment();
             }
         }
     }
