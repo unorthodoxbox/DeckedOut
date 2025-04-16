4f329d9a6e0e84a34c31331fa9405eecbe5684fa
Divesh Gupta <diveshg9@gmail.com>
2025-04-15 23:32:21 -0400

    fixed weapons shootpoint and direction

--- Assets/Scripts/Shooting/Bullet.cs
+++ Assets/Scripts/Shooting/Bullet.cs
@@ -2,40 +2,27 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject player;
 
     private float damage = 0f;
     private Rigidbody rb;
 
-    void Awake()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, 10f);
     }
-
-
-    //Better solution is an invisible box around map, but that can wait till map actually exists
-    void Update() {
-        if (gameObject.transform.position.x - player.transform.position.x > 50 || gameObject.transform.position.y - player.transform.position.y > 50 || gameObject.transform.position.z - player.transform.position.z > 50) {
-            Destroy(gameObject);
-        }
-    }
-    
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Enemy" && gameObject.tag == "Player")
+        if (other.gameObject.tag == "Enemy")
         {
             other.gameObject.GetComponent<EntityStats>().takeDamage(damage);
         }
         Destroy(gameObject);
     }
 
-    public void InitializeVariables(string tag, float damage, float speed, Vector3 direction)
+    public void InitializeVariables(float damage)
     {
-        gameObject.tag = tag;
         this.damage = damage;
-
-        // Move the bullet using Rigidbody physics
-        rb.linearVelocity = direction * speed;
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,7 @@ public class Weapon : MonoBehaviour
 {
     public bool isRanged = true;
     public GameObject bulletPrefab;
+    public float bulletSpeed;
     public Transform firePoint;
     public float cooldown = 0.2f;
     private float lastShotTime = 0f;
@@ -46,16 +47,31 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Vector3 shootDir = cam.transform.forward.normalized;
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
 
-        bullet.GetComponent<Bullet>().InitializeVariables(
-            gameObject.tag,
-            playerStats.attackDamage,
-            playerStats.bulletSpeed,
-            shootDir
-        );
+        Vector3 targetPoint;
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f)); // Center of screen
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000f))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(1000f); // Just shoot straight forward
+        }
+
+        Vector3 direction = (targetPoint - firePoint.position).normalized;
+
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = direction * bulletSpeed;
+        }
+
+        // Optional: call a method on the bullet to pass damage
+        bullet.GetComponent<Bullet>().InitializeVariables(playerStats.attackDamage);
     }
+
 
     System.Collections.IEnumerator Swing()
     {
