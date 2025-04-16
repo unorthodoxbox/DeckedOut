b17054ccce1607f85692f655efa4e24dcbe086b7
Wyatt Wolf <wyattawolf@gmail.com>
2025-04-16 15:11:07 -0400

    Card Spinning

--- Assets/Scripts/Cards/CardObject.cs
+++ Assets/Scripts/Cards/CardObject.cs
@@ -4,6 +4,26 @@ public class CardObject : MonoBehaviour
 {
     public Card cardData;  // The ScriptableObject data for the card
 
+    private Vector3 startPos;
+    public float floatAmplitude = 0.25f;   // How high it floats
+    public float floatFrequency = 1f;      // How fast it floats
+    public float spinSpeed = 30f;          // How fast it spins
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        // Floating motion
+        float newY = startPos.y + Mathf.Sin(Time.time * floatFrequency) * floatAmplitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+        // Spinning
+        transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime, Space.World);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -12,7 +32,4 @@ public class CardObject : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-    
 }
-
-
