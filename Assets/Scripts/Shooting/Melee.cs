643a4ec87c2997aea268025e8dcd7dad2aa45c19
umr34 <114027589+umr34@users.noreply.github.com>
2025-04-23 18:30:27 -0400

    Melee *should* make noise

--- Assets/Scripts/Shooting/Melee.cs
+++ Assets/Scripts/Shooting/Melee.cs
@@ -3,22 +3,13 @@ using UnityEngine;
 public class Melee : MonoBehaviour
 {
     float damage = 10f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Enemy")
         {
             other.gameObject.GetComponent<EntityStats>().takeDamage(damage);
+            AudioManager.weaponPlayer.GetComponent<SoundPlayer>().Play("Melee Swing");
         }
     }
 }
